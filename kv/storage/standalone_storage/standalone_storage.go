5e3c53e04078efe09787d64eed601dac5d8ccf83
chx <chx2502@gmail.com>
2021-06-15 18:41:03 +0800

    project1 passed
    Former-commit-id: d6bb890e452c5e045d9740d9634d3bd657598704

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,20 +1,25 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	// Your Data Here (1).
+	// Your Code Here (1).
+	db *badger.DB
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	return &StandAloneStorage{
+		db: engine_util.CreateDB("/tmp/badger", conf.Raft),
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -23,16 +28,56 @@ func (s *StandAloneStorage) Start() error {
 }
 
 func (s *StandAloneStorage) Stop() error {
-	// Your Code Here (1).
-	return nil
+	return s.db.Close()
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
-	// Your Code Here (1).
-	return nil, nil
+	txn := s.db.NewTransaction(false)
+	return &StandAloneStorageReader{txn}, nil
 }
 
+// 调用 badger.DB.NewTransaction() 对数据进行操作
+// lab1 没有使用到 ctx
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	txn := s.db.NewTransaction(true)
+	for _, modify := range batch {
+		switch modify.Data.(type) {
+		case storage.Put:
+			put := modify.Data.(storage.Put)
+			if err := txn.Set(engine_util.KeyWithCF(put.Cf, put.Key), put.Value); err != nil {
+				return err
+			}
+
+		case storage.Delete:
+			del := modify.Data.(storage.Delete)
+			if err := txn.Delete(engine_util.KeyWithCF(del.Cf, del.Key)); err != nil {
+				return err
+			}
+		}
+	}
+	if err := txn.Commit(); err != nil {
+		return err
+	}
 	return nil
+}
+
+type StandAloneStorageReader struct {
+	txn *badger.Txn
+}
+
+func (reader *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(reader.txn, cf, key)
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	}
+	return val, err
+}
+
+func (reader *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, reader.txn)
+}
+
+func (reader *StandAloneStorageReader) Close() {
+	reader.txn.Discard()
 }
